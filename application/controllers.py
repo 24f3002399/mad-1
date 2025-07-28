6aa0f85011735efc603a593496f164437876f007
24f3002399 <24f3002399@ds.study.iitm.ac.in>
2025-07-28 12:25:31 +0530

    application

--- application/controllers.py
+++ application/controllers.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, redirect,url_for, request
 from flask import current_app as app
-
 from .models import *
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("Agg")
 
 @app.route("/login" , methods = ["POST" , "GET"])
 def login():
@@ -14,12 +17,11 @@ def login():
                 if (this_user.type == "admin"):
                     return redirect("/admin")
                 else:
-                    return render_template("user_home.html",this_user = this_user)
+                    return redirect(f"/Home_user/{this_user.id}")
             else:
-                return "Password is Wrong"
+                return render_template("incorrect_pass.html")
         else:
-           return "User does not exist"
-
+           return render_template("not_exist.html")
     return render_template("login.html")
 
 @app.route("/register" ,methods = ["GET" ,"POST"])
@@ -32,33 +34,18 @@ def register():
         pin_code = request.form.get("pin")
         user_email = Users.query.filter_by(email = email).first()
         if user_email :
-            return "user already exist"
+            return render_template("already_exist.html")
         else:
             user = Users(name= name, email = email , password = password , address = address , pincode = pin_code,)
             db.session.add(user)
             db.session.commit()
-        return "Registration successfully "
+            return redirect("/login")
     return render_template("register.html")
 
-@app.route("/login/<int:id>")
-def adhome(id):
-    this_user = Users.query.filter_by(id = id).first()    
-    return render_template("admin_home.html" , this_user = this_user)
-
-@app.route("/admin_user/<int:id>")
-def admin_user(id):
-    this_user = Users.query.filter_by(id = id).first()
-    return render_template("admin_users.html", this_user = this_user)
-
-@app.route("/search/<int:id>")
-def search(id):
-    this_user = Users.query.filter_by(id = id).first()
+@app.route("/admin_search")
+def search():
+    this_user = Users.query.filter_by(type = "admin").first()
     return render_template("admin_search.html" , this_user = this_user)
-
-@app.route("/summary/<int:id>")
-def summary(id):
-    this_user = Users.query.filter_by(id = id).first()
-    return render_template("admin_summary.html" , this_user = this_user)
 
 @app.route("/prof_edit/<int:id>" , methods = ["GET", "POST"])
 def prof_edit(id):
@@ -70,7 +57,7 @@ def prof_edit(id):
         this_user.address = request.form.get("add")
         this_user.pin_code = request.form.get("pin")
         db.session.commit()
-        return "Profile Edit Successfully"
+        return render_template("pfile_edit.html" , this_user = this_user)
     return render_template("prof_edit.html" , this_user = this_user)
 
 @app.route("/add_lot/<int:id>" ,methods = ["GET", "POST"])
@@ -120,13 +107,34 @@ def edit_lot(id):
 
 @app.route("/Home_user/<int:id>")
 def Ushome(id):
-    this_user = Users.query.filter_by(id = id).first()    
-    return render_template("user_home.html" , this_user = this_user)
+    this_user = Users.query.filter_by(id = id).first()
+    reserve = Reserve_parking_spot.query.filter_by(user_id = id).all()
+    return render_template("user_home.html" , this_user = this_user, reserve = reserve)
 
 @app.route("/U_summary/<int:id>")
 def Us_summary(id):
     this_user = Users.query.filter_by(id = id).first()
-    return render_template("user_summary.html" , this_user = this_user)
+    a_s = len(Reserve_parking_spot.query.filter_by(status = "released" , user_id = id).all())
+    o_s = len(Reserve_parking_spot.query.filter_by(status = "occupied" , user_id = id).all())
+
+    labels = ["Parked Out" , "Occupied"]
+    sizes = [a_s , o_s]
+    colors = ["yellow" , "green"]
+    plt.pie(sizes , labels = labels , colors = colors ,autopct = "%1.1f%%")
+    plt.title("Parking Details")
+    plt.savefig(f"static/{id}_us_pie.png")
+    plt.clf()
+
+    labels = ["Parked Out" , "Occupied"]
+    sizes = [a_s , o_s]
+    plt.bar(labels , sizes)
+    plt.xlabel("Parking Details")
+    plt.ylabel("No of Spot")
+    plt.title("Parking Details")
+    plt.savefig(f"static/{id}_us_bar.png")
+    plt.clf()
+
+    return render_template("user_summary.html" , this_user = this_user , a_s = a_s , o_s = o_s)
 
 @app.route("/us_prof_edit/<int:id>" , methods = ["GET", "POST"])
 def us_prof_edit(id):
@@ -136,19 +144,36 @@ def us_prof_edit(id):
         this_user.password = request.form.get("pwd")
         this_user.name = request.form.get("name")
         this_user.address = request.form.get("add")
-        this_user.pin_code = request.form.get("pin")
+        this_user.pincode = request.form.get("pin")
         db.session.commit()
-        return "Profile Edit Successfully"
+        return render_template("pfile_edit.html", this_user = this_user)
     return render_template("us_prof_edit.html" , this_user = this_user)
 
-@app.route("/release/<int:id>")
-def release(id):
-    this_user = Users.query.filter_by(id = id).first()
-    return render_template("release_prk.html" , this_user = this_user)
+@app.route("/release/<int:user_id>/<int:spot_id>/<int:lot_id>" , methods = ["GET", "POST"])
+def release(user_id , spot_id , lot_id):
+    this_user = Users.query.filter_by(id = user_id).first()
+    spot = Parking_spot.query.filter_by(id = spot_id).first()
+    lot = Parking_lot.query.filter_by(id = lot_id).first()
+    l_time = datetime.datetime.now()
+    l_stamp = l_time - spot.parking_time
+    hour = (l_stamp.total_seconds())/3600
+    price = (hour*lot.Price)
+    if request.method == "POST":
+        
+        res = Reserve_parking_spot.query.filter_by(spot_id = spot.id).first()
+        res.status = "released"
+        res.leaving_time = l_time
+        res.parking_cost = price
+        db.session.commit()
 
-@app.route("/book")
-def s_book():
-    return render_template("book_prk.html")
+        spot.status = "available"
+        spot.occupied_user = None
+        spot.vehicle_no = None
+        spot.parking_time = None
+        lot.occupied_spot -= 1 
+        db.session.commit()
+        return redirect(f"/Home_user/{user_id}")
+    return render_template("release_prk.html" , this_user = this_user, spot = spot , l_time = l_time ,price = price)
 
 @app.route("/admin")
 def admin():
@@ -174,7 +199,11 @@ def delete_lot(id):
     this_lot = Parking_lot.query.filter_by(id = id).first()
     if request.method == "POST":
         lot = Parking_lot.query.get(id)
-        if lot.occupied_lot == None:
+        if lot.occupied_spot == None:
+            db.session.delete(lot)
+            db.session.commit()
+            return redirect("/admin")
+        elif lot.occupied_spot == 0:
             db.session.delete(lot)
             db.session.commit()
             return redirect("/admin")
@@ -185,6 +214,91 @@ def delete_lot(id):
 @app.route("/occupied/<int:lot_id>/<int:spot_id>")
 def occupied(lot_id ,spot_id):
     this_lot = Parking_lot.query.filter_by(id = lot_id).first()
-    spot_id = Parking_spot.query.filter_by(id = spot_id).first()    
-    return render_template("occupied_prk.html" , this_lot = this_lot , spot_id = spot_id ) 
+    spot = Parking_spot.query.filter_by(id = spot_id).first()    
+    return render_template("occupied_prk.html" , this_lot = this_lot , spot = spot ) 
+
+@app.route("/users")
+def users():
+    this_user = Users.query.filter_by(type = "admin").first()
+    users = Users.query.filter_by(type = "general")
+    return render_template("admin_users.html" , users = users , this_user = this_user)
+
+@app.route("/ad_search")
+def ad_search():
+    search_word = request.args.get("search")
+    key = request.args.get("key")
+    if key == "user":
+        result = Users.query.filter_by(id = search_word).first()
+        spot = Parking_spot.query.filter_by(occupied_user = search_word).all()
+        tot_spot = len(spot)
+        return render_template("ad_search.html" ,result = result , key = key ,spot = spot , tot_spot = tot_spot)
+
+    elif key == "parking_lot":
+        result = Parking_lot.query.filter_by(Address = search_word).all()
+        spots = Parking_spot.query.all()
+        return render_template("ad_search.html" ,key = key ,spots = spots ,result = result)
+    else:
+        return redirect("/admin_search")
+
+@app.route("/us_search/<int:id>")
+def us_search(id):
+    this_user = Users.query.filter_by(id = id).first()
+    search_word = request.args.get("search")
+    key = request.args.get("key")
+    if key == "location":
+        result = Parking_lot.query.filter_by(prime_location_name = search_word).all()
+        return render_template("us_search.html" , result = result , key = key , search_word = search_word , this_user = this_user)
+    else:
+        result = Parking_lot.query.filter_by(Pin_code = search_word).all()
+        return render_template("us_search.html" , result = result , key = key , search_word = search_word , this_user = this_user)
+
+@app.route("/book/<int:user_id>/<int:lot_id>" , methods = ["GET" , "POST"])
+def s_book(user_id , lot_id):
+    user = Users.query.filter_by(id = user_id).first()
+    lot = Parking_lot.query.filter_by(id = lot_id).first()
+    spot = Parking_spot.query.filter_by(lot_id = lot_id , status = "available").first()
+    v_no = request.form.get("v_no")
+    if request.method == "POST":  
+        spot.status = "occupied"
+        spot.occupied_user = user.id
+        if lot.occupied_spot == None:
+            lot.occupied_spot = 1
+        else:
+            lot.occupied_spot += 1
+        db.session.commit()
+        p_time = datetime.datetime.now()
+        spot.vehicle_no = v_no
+        spot.parking_time = p_time
+        db.session.commit()
+        reserve = Reserve_parking_spot(spot_id = spot.id , status = "occupied" , user_id = user.id , vehicle_no = v_no , parking_time = p_time , location = lot.prime_location_name , lot_id = lot.id)
+        db.session.add(reserve)
+        db.session.commit()        
+        return redirect(f"/Home_user/{user.id}")
+    return render_template("book_prk.html" , user = user , lot = lot , spot = spot)
+
+@app.route("/ad_summary")
+def ad_summary():
+    this_user = Users.query.filter_by(type = "admin").first()
+    a_s = len(Parking_spot.query.filter_by(status = "available").all())
+    o_s = len(Parking_spot.query.filter_by(status = "occupied").all())
+
+    labels = ["Available" , "Occupied"]
+    sizes = [a_s , o_s]
+    colors = ["green" , "yellow"]
+    plt.pie(sizes , labels = labels , colors = colors ,autopct = "%1.1f%%")
+    plt.title("Status of Parking Spot")
+    plt.savefig("static/ad_pie.png")
+    plt.clf()
+
+    labels = ["Available" , "Occupied"]
+    sizes = [a_s , o_s]
+    plt.bar(labels , sizes)
+    plt.xlabel("Status of Parking Spot")
+    plt.ylabel("No of Spot")
+    plt.title("Status of Parking Spot")
+    plt.savefig("static/ad_bar.png")
+    plt.clf()
+
+    return render_template("admin_summary.html" , a_s = a_s , o_s = o_s ,this_user = this_user)
+
 
--- application/models.py
+++ application/models.py
@@ -7,21 +7,16 @@ class Parking_lot(db.Model):
     Address = db.Column(db.String())
     Pin_code = db.Column(db.Integer , nullable = False)
     maximum_number_of_spots = db.Column(db.Integer)
-    spot = db.relationship('Parking_spot' ,cascade = 'all,delete ,delete-orphan', backref = 'bearer')
-    occupied_lot = db.Column(db.Integer)
+    spot = db.relationship('Parking_spot' ,cascade = 'all,delete ,delete-orphan', backref = 'parking_lot')
+    occupied_spot = db.Column(db.Integer)
 
 class Parking_spot(db.Model):
     id = db.Column(db.Integer, primary_key= True)
     lot_id = db.Column(db.Integer, db.ForeignKey('parking_lot.id') ,nullable = False )
     status = db.Column(db.String(), default = 'available')
-
-class Reserve_parking_spot(db.Model):
-    id = db.Column(db.Integer , primary_key = True)
-    spot_id = db.Column(db.Integer, db.ForeignKey('parking_spot.id'))
-    user_id = db.Column(db.Integer , db.ForeignKey('users.id'), nullable= False)
-    parking_timestamp = db.Column(db.Numeric() )
-    leaving_timestamp = db.Column(db.Numeric())
-    parking_cost = db.Column(db.Integer)
+    occupied_user = db.Column(db.Integer , db.ForeignKey('users.id'))
+    vehicle_no = db.Column(db.String())
+    parking_time = db.Column(db.DateTime())
 
 class Users(db.Model):
     id = db.Column(db.Integer , primary_key = True)
@@ -31,3 +26,16 @@ class Users(db.Model):
     address = db.Column(db.String())
     pincode = db.Column(db.Integer)
     type = db.Column(db.String() ,nullable = False ,default = "general")
+    spot = db.relationship('Parking_spot' ,cascade = 'all,delete ,delete-orphan', backref = 'users')
+
+class Reserve_parking_spot(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    spot_id = db.Column(db.Integer, db.ForeignKey('parking_spot.id') )
+    status = db.Column(db.String(), default = 'available')
+    user_id = db.Column(db.Integer , db.ForeignKey('users.id'))
+    location = db.Column(db.String() , db.ForeignKey('parking_lot.prime_location_name'))
+    vehicle_no = db.Column(db.String())
+    parking_time = db.Column(db.DateTime())
+    leaving_time = db.Column(db.DateTime())
+    parking_cost = db.Column(db.Integer)
+    lot_id = db.Column(db.Integer, db.ForeignKey('parking_lot.id'))
